099438afe5c5b581f06f8a1df6cc0b74473ab6a9
Pranali Pawar <pawar.1@iitj.ac.in>
2018-02-06 18:56:21 +0530

    Add files via upload

---
+++ HackerRank/Algorithms/CamelCase.cpp
@@ -0,0 +1,38 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    string s;
+    cin >> s;
+    int count = 1 ;
+    for(int i=1;i<s.size()-1;i++){
+        if(isupper(s[i])){
+            count++;
+        }
+    }
+    cout << count ;
+    return 0;
+}

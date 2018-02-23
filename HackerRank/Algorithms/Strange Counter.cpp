d3f7da022a5185e47c64194ef587381eda9f93ed
Pranali Pawar <pawar.1@iitj.ac.in>
2018-02-23 23:44:05 +0530

    Add files via upload

---
+++ HackerRank/Algorithms/Strange Counter.cpp
@@ -0,0 +1,34 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    long long k=3,t; cin >> t;
+while (k*2<=t+2)
+    k*=2;
+
+cout << k - (t-(k-2));
+    return 0;
+}

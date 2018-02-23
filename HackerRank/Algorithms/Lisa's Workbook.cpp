c2f3f20816a4806c77f3c25aeb3d7cb2eeb8d299
Pranali Pawar <pawar.1@iitj.ac.in>
2018-02-23 23:44:02 +0530

    Add files via upload

---
+++ HackerRank/Algorithms/Lisa's Workbook.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n, k;
+cin>>n>>k;
+int t, special=0, pageN = 1, probN = 1;
+cin>>t;
+int chap = 1;
+while (chap <=n){
+    if (probN<=pageN && pageN <= (probN+k-1 < t ? probN+k-1 : t))
+        special++;
+    pageN++;
+    probN+=k;
+    if (probN>t){
+        probN=1;
+        chap++;
+        cin>>t;
+    }
+}
+cout<<special<<endl;
+return 0;
+}
